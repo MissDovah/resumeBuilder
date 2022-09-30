dcada8fc388f57123469311ce664e737c05f2c03
MissDovah <mwhite.prsvr@gmail.com>
2022-09-30 09:24:25 -0500

    Third push

--- resume-builder-app/src/App.js
+++ resume-builder-app/src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <h1> Monica R White </h1>
       <img src="C:\Code-White\Code-White\resumeBuilder\resume-builder-app\src\skyrimcool.jpg" alt= "second git push"/>  
+      <p> This is the start-up of a resume in REACT. </p>
     </div>
   );
 }
