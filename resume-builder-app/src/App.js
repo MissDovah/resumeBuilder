6d82d59f3b6fc7725905a209a40aeadd56c6a80b
MissDovah <mwhite.prsvr@gmail.com>
2022-09-30 09:22:57 -0500

    Second push

--- resume-builder-app/src/App.js
+++ resume-builder-app/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1> Monica R White </h1>
-        
+      <img src="C:\Code-White\Code-White\resumeBuilder\resume-builder-app\src\skyrimcool.jpg" alt= "second git push"/>  
     </div>
   );
 }
